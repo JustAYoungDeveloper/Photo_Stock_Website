cce989cb01dc81688904e61dbf4d5ffb9a674075
Solat <solatpukhtunyar123@gmail.com>
2025-01-17 20:22:49 +0500

    The error is still there.

--- js/global.js
+++ js/global.js
@@ -1,6 +1,23 @@
 /**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
+ * Import
  */
 
-"use strict";
+import { ripple } from "./utils/ripple.js";
+
+/**
+ * Header on Scroll State
+ */
+
+const /* {NodeElement} */ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Add ripple effect
+ */
+
+const /** {NodeList} */ $rippleElems =
+    document.querySelectorAll("[data-ripple]");
+$rippleElems.forEach(($rippleElem) => ripple($rippleElem));
